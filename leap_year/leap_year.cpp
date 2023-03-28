23e57babf2ecef32b62a9013df68d29fda3c6444
RandomGuy <codymoores@live.com>
2023-03-28 12:21:07 -0230

    Added leap year

---
+++ leap_year/leap_year.cpp
@@ -0,0 +1,37 @@
+/* Task:
+    ! Write a leap_year.cpp program that:
+        * Takes a year as input.
+        * Checks to see if the year is a four-digit number.
+        * Displays whether or not the year falls on a leap year.
+
+    ! There are 3 criteria that must be taken into account to identify a leap year:
+        * If the year can be evenly divided by 4 then it is a leap year, however…
+        * If that year can be evenly divided by 100, and it is not evenly divided by 400, then it is NOT a leap year.
+        * If that year is evenly divisible by 400, then it is a leap year.
+*/
+
+#include <iostream>
+
+int main()
+{
+    // Takes a year as input:
+    int y;
+    std::cout << "Input a year: ";
+    std::cin >> y;
+
+    // if y / 4 = 0                         -> leap year
+    // if y / 100 = 0 and y / 400 not=0     -> not leap year
+    // if y % 400 = 0                       -> leap year
+    if (y <= 1000 || y >= 9999)
+    {
+        std::cout << "Invalid Year.\n";
+    }
+    else if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
+    {
+        std::cout << "The year " << y << " is a leap year!\n";
+    }
+    else
+    {
+        std::cout << "Not a leap year.\n";
+    }
+}
